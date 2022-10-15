9cf4d252e82ad9cde378f23dc2cbfb0feaa8a1ad
Yudai Nakajima <kituneudon97@gmail.com>
2022-10-15 16:32:24 +0900

    #4 タスクを保存できるようにした

--- app/Http/Controllers/NormalUser/TaskController.php
+++ app/Http/Controllers/NormalUser/TaskController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers\NormalUser;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tasks;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
     public function index()
     {
         return view('NormalUser.tasks');
+    }
+
+    public function store(Request $request)
+    {
+        $tasks = new Tasks();
+
+        $tasks->create([
+            'title' => $request->input('task_title'),
+            'description' => $request->input('task_description'),
+            'user_id' => Auth::id()
+        ]);
+
+        return redirect('/normal-user/tasks');
     }
 }
--- resources/views/NormalUser/tasks.blade.php
+++ resources/views/NormalUser/tasks.blade.php
@@ -1,1 +1,7 @@
 通常ユーザ用ページだよ
+<form action="/normal-user/tasks/store" method="post">
+    @csrf
+    <input type="text" name="task_title">
+    <textarea name="task_description" id="task_description" cols="30" rows="10"></textarea>
+    <button type="submit">追加</button>
+</form>
