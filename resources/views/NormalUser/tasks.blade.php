037ba3d61807eb47831fc36268f8a825e66a3f0d
Yudai Nakajima <kituneudon97@gmail.com>
2022-10-15 18:16:01 +0900

    #18 最低限タスク一覧の位置を調整

--- resources/views/NormalUser/tasks.blade.php
+++ resources/views/NormalUser/tasks.blade.php
@@ -1,13 +1,21 @@
-通常ユーザ用ページだよ
-<form action="/normal-user/tasks/store" method="post">
-    @csrf
-    <input type="text" name="task_title">
-    <textarea name="task_description" id="task_description" cols="30" rows="10"></textarea>
-    <button type="submit">追加</button>
-</form>
-@foreach ($tasks as $task)
-    <div>
-        <p>{{ $task['title'] }}</p>
-        <p>{{ $task['description'] }}</p>
-    </div>
-@endforeach
+<x-app-layout>
+    通常ユーザ用ページだよ
+    <form action="/normal-user/tasks/store" method="post">
+        @csrf
+        <input type="text" name="task_title">
+        <textarea name="task_description" id="task_description" cols="30" rows="10"></textarea>
+        <button type="submit">追加</button>
+    </form>
+    @foreach ($tasks as $task)
+        <div class="m-4">
+            <div class="flex flex-row itmes-center">
+                <input class="m-1" type="checkbox">
+                <div>{{ $task['title'] }}</div>
+            </div>
+            <div class="flex">
+                <p class="mr-2">詳細</p>
+                <p>{{ $task['description'] }}</p>
+            </div>
+        </div>
+    @endforeach
+</x-app-layout>
