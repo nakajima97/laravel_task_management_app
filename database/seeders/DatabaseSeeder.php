14cc70736ebd867c76c5ace60cccdfb832cb7e8b
Nakajima <kituneudon97@gmail.com>
2022-11-12 15:40:26 +0900

    #34 ユーザ作成のseederを用意した (#37)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\TypeSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,7 +23,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            TypeSeeder::class
+            TypeSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
